2bd61dc7a74a2fec6201be62346c9867ce99529d
Joey Quandt <joey.r.quandt@gmail.com>
2026-07-17 15:36:27 +0200

    fix: build error

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -139,7 +139,7 @@ else:
 
 # Production security
 if not DEBUG:
-    SECURE_SSL_REDIRECT = os.getenv("SECURE_SSL_REDIRECT", "True").lower() in ("true", "1")
+    SECURE_SSL_REDIRECT = os.getenv("SECURE_SSL_REDIRECT", "False").lower() in ("true", "1")
     SECURE_HSTS_SECONDS = 31536000
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_HSTS_PRELOAD = True
